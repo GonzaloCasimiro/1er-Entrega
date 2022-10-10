ee78dd3ea83723ee2a526dc8edb78e75c498b8df
Gonzalo Casimiro <gonzaloismaelcasimiro@gmail.com>
2022-10-10 16:27:53 -0300

    agregue funciones a la 1era entrega

--- login.js
+++ login.js
@@ -1,22 +1,23 @@
 /// Registro una cuenta de usuario
 alert("Para ingresar debes registrar una cuenta")
 let nombre_de_usuario=prompt("Elige tu nombre de usuario");
-let contraseña_de_usuario=prompt("elige tu contraseña");
+let contraseña_de_usuario=prompt("Elige tu contraseña");
 alert("Felicitaciones,tu cuenta ha sido creada,ya puedes acceder a tu cuenta");
 // PIDO NOMBRE DE USUARIO Y CONTRASEÑA
-
 let usuario =prompt("ingrese nombre de usuario");
 let contraseña =prompt("ingrese contraseña");
-// EJECUTO SEGUN CONDICIONES
-if (usuario==nombre_de_usuario && contraseña==contraseña_de_usuario){
-    //DOY ACCESO A CUENTA
-    alert("bienvenido "+usuario);
-}
-else{
-    while(usuario!=nombre_de_usuario || contraseña!=contraseña_de_usuario){
-        alert("usuario o contraseña incorrecta,por favor vuelva a ingresar");
-        usuario=prompt("ingrese nombre de usuario");
-        contraseña=prompt("ingrese contraseña");
+// CREO FUNCION QUE VERIFIQUE LOS VALORES INGRESADOS Y DE ACCESO SEGUN LAS CONDICIONALES
+function verificando_cuenta(usuario,contraseña){
+    if(usuario==nombre_de_usuario&& contraseña==contraseña_de_usuario){
+        return alert("Bienvenido "+usuario);
     }
-    alert("bienvenido "+ usuario);
+    else{
+        while(usuario!=nombre_de_usuario || contraseña!=contraseña_de_usuario){
+            alert("usuario o contraseña incorrecta,por favor vuelva a ingresar");
+            usuario=prompt("ingrese nombre de usuario");
+            contraseña=prompt("ingrese contraseña");
+        }
+        return alert("bienvenido "+ usuario)
+    }
 }
+verificando_cuenta(usuario,contraseña);
